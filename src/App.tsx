fc5ab4a931862a6eceec295a6c0eded30ee73b83
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-19 12:48:55 +0100

    Import Footer component & add top padding for content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Footer from './components/Footer';
 import Header from './components/Header';
 
 export default function App() {
     return (
         <>
             <Header />
+            <div className="pt-[70px] lg:pt-[86px] xl:pt-[102px]"></div>
+            <Footer />
         </>
     );
 }
