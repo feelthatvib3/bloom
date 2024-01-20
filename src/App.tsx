bba20da2b4e8b0c23acc5a63bcc0d2d703e6ea3e
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-20 19:55:35 +0100

    Define routes for the home & not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
     return (
         <>
             <Header />
-            <div className="pt-[70px] lg:pt-[86px] xl:pt-[102px]"></div>
+            <div className="pt-[70px] lg:pt-[86px] xl:pt-[102px]">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </div>
             <Footer />
         </>
     );
