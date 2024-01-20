8b6acbb817adbb0c0b2fc8afa7b63e683f4c5b6a
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-20 19:56:04 +0100

    Create NotFound page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,24 @@
+import errorImage from '../assets/404.svg';
+import Button from '../components/Button';
+import Container from '../components/Container';
+import Title from '../components/Title';
+
+export default function NotFoundPage() {
+    return (
+        <main>
+            <Container className="flex max-w-screen-lg flex-col items-center justify-center py-20 text-center">
+                <img src={errorImage} alt="404 Error" />
+                <div className="py-8">
+                    <Title>Page Not Found</Title>
+                    <p className="text-darkgray pt-4 text-xl">
+                        We're sorry, the page you requested could not be found.
+                        Please go back to the homepage.
+                    </p>
+                </div>
+                <Button href="/" className="max-w-fit">
+                    Go Home
+                </Button>
+            </Container>
+        </main>
+    );
+}
