65fd977277684836af61ab9415f00b47fd75f3fe
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-24 00:04:29 +0100

    Add products reducer to the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
 import { CategoriesSlice } from './features/categoriesSlice';
+import { ProductsSlice } from './features/productsSlice';
 
 export const store = configureStore({
     reducer: {
         categories: CategoriesSlice.reducer,
+        products: ProductsSlice.reducer,
     },
 });
 
