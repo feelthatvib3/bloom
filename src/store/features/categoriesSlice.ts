87d5afdca0cff9a9865045f19d08d87922c15568
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-23 15:49:24 +0100

    Remove unused imports

--- src/store/features/categoriesSlice.ts
+++ src/store/features/categoriesSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 export interface Category {
     id: number;
