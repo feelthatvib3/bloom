5d40315fde101c945b0862522a3f0e2c99c378b2
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-24 17:04:44 +0100

    Define CategoriesPage route

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
+import CategoriesPage from './pages/CategoriesPage';
 
 export default function App() {
     return (
         <>
             <Header />
-            <div className="pt-[70px] lg:pt-[86px] xl:pt-[102px]">
+            <div className="pt-[70px] lg:pt-[110px] xl:pt-[126px]">
                 <Routes>
                     <Route path="/" element={<HomePage />} />
+                    <Route path="/categories" element={<CategoriesPage />} />
                     <Route path="*" element={<NotFoundPage />} />
                 </Routes>
             </div>
