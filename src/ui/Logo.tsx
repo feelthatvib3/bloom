30ae70c125f204e2d83fc0463cefdbcdb64fb846
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-25 15:23:47 +0100

    Replace <a> with <Link> component of react-router-dom lib

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -1,13 +1,14 @@
+import { Link } from 'react-router-dom';
 import logoImage from '../assets/logo.svg';
 
 export default function Logo() {
     return (
-        <a href="/">
+        <Link to="/">
             <img
                 src={logoImage}
                 alt="React Garden Logo"
                 className="h-11 w-11 lg:h-[70px] lg:w-[70px]"
             />
-        </a>
+        </Link>
     );
 }
