6efe47c8ead7f1abf56461681735365ee73d6cfe
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-25 19:38:57 +0100

    Extend logic to calculate the discount percentage via calculateDiscountPercentage utility function

--- src/ui/DiscountBadge.tsx
+++ src/ui/DiscountBadge.tsx
@@ -1,11 +1,25 @@
+import calculateDiscountPercentage from '../utils/calculateDiscountPercentage';
+
 interface DiscountProps {
-    discountValue: number;
+    regularPrice: number;
+    discountPrice: number;
+    className?: string;
 }
 
-export default function DiscountBadge({ discountValue }: DiscountProps) {
+export default function DiscountBadge({
+    regularPrice,
+    discountPrice,
+    className = '',
+}: DiscountProps) {
+    const discountPercentage = calculateDiscountPercentage(
+        regularPrice,
+        discountPrice,
+    );
     return (
-        <div className="rounded-md bg-accent px-2 py-1 text-xl font-semibold text-white">
-            -{Math.floor(discountValue)}%
+        <div
+            className={`rounded-md bg-accent px-2 py-1 text-xl font-semibold text-white ${className}`}
+        >
+            {discountPercentage}%
         </div>
     );
 }
