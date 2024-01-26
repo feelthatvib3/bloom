a00e548293758de39bba4a7618bba5e5d30fc940
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-26 22:13:36 +0100

    Remove unused imports

--- src/store/features/productsSlice.ts
+++ src/store/features/productsSlice.ts
@@ -1,5 +1,4 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { RootState, useAppDispatch } from '../index';
 import { Category } from './categoriesSlice';
 
 export interface Product {
