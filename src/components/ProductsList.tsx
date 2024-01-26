b5c844d44d8a817c403f47246ab22cf71abba087
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-26 21:17:58 +0100

    Clear products on component unmount

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import {
+    clearProducts,
     fetchAllProducts,
     fetchDiscountedProducts,
     fetchProductsByCategoryId,
 } from '../store/features/productsSlice';
 import ProductCard from '../ui/ProductCard';
 import { RootState, useAppDispatch, useAppSelector } from '../store';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 interface ProductsListProps {
     type: 'category' | 'products' | 'sales';
@@ -16,16 +17,20 @@ export default function ProductsList({ type }: ProductsListProps) {
     const { products } = useAppSelector((state: RootState) => state.products);
     const dispatch = useAppDispatch();
     const { categoryId } = useParams();
-    const { pathname } = useLocation();
+
     useEffect(() => {
         if (type === 'products') {
             dispatch(fetchAllProducts());
-        } else if (type === 'category') {
-            dispatch(fetchProductsByCategoryId(categoryId!));
+        } else if (type === 'category' && categoryId) {
+            dispatch(fetchProductsByCategoryId(categoryId));
         } else if (type === 'sales') {
             dispatch(fetchDiscountedProducts());
         }
-    }, [pathname]);
+
+        return () => {
+            dispatch(clearProducts());
+        };
+    }, [type]);
     return (
         <ul className="mt-6 grid gap-5 md:grid-cols-2 lg:mt-8 xl:grid-cols-4 2xl:mt-10 2xl:gap-8">
             {products.map((product) => (
