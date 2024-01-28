e909a0dad160929fa8a919e95dd35e9dceaa5567
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-28 16:33:22 +0100

    Add state for handling increase/decrese/setAmount events

--- src/ui/ProductAmountCounter.tsx
+++ src/ui/ProductAmountCounter.tsx
@@ -1,16 +1,47 @@
+import { ChangeEvent, useState } from 'react';
+
 export default function ProductAmountConter() {
+    const [productAmount, setProductAmount] = useState<number>(1);
+
+    const handleIncreaseAmountButton = () =>
+        setProductAmount(productAmount + 1);
+
+    const handleDecreaseAmountButton = () => {
+        if (productAmount === 1) {
+            return;
+        } else {
+            setProductAmount(productAmount - 1);
+        }
+    };
+
+    const handleInputAmount = (event: ChangeEvent<HTMLInputElement>) => {
+        if (+event.target.value <= 0) {
+            setProductAmount(1);
+        } else {
+            setProductAmount(+event.target.value);
+        }
+    };
+
     return (
         <div className="flex">
-            <button className="z-10 flex w-[47px] items-center justify-center rounded-md border border-divider bg-white">
+            <button
+                onClick={handleDecreaseAmountButton}
+                className="z-10 flex w-[47px] items-center justify-center rounded-md border border-divider bg-white"
+            >
                 <span className="h-[2px] w-4 rounded-full bg-darkgray"></span>
             </button>
             <input
-                className="no-spinner z-0 max-w-[70px] -translate-x-2 rounded-md border border-divider bg-white px-1 py-3 text-center text-lg font-semibold focus:outline-none"
+                className="no-spinner z-0 max-w-[70px] -translate-x-2 rounded-md border border-divider bg-white px-3 py-3 text-center text-lg font-semibold focus:outline-none"
                 min={1}
-                defaultValue={1}
+                defaultValue={productAmount}
+                value={productAmount}
+                onChange={handleInputAmount}
                 type="number"
             />
-            <button className="z-10 flex w-[47px] -translate-x-4 items-center justify-center rounded-md border border-divider bg-white">
+            <button
+                onClick={handleIncreaseAmountButton}
+                className="z-10 flex w-[47px] -translate-x-4 items-center justify-center rounded-md border border-divider bg-white"
+            >
                 <span className="h-[2px] w-4 translate-x-1/2 rounded-full bg-darkgray"></span>
                 <span className="h-[2px] w-4 -translate-x-1/2 rotate-90 rounded-full bg-darkgray"></span>
             </button>
