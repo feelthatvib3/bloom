c540fec878865abb8fadd21f1aa548273d5aeeab
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-31 23:01:13 +0100

    Retrieve amount of products added from redux store

--- src/components/ui/CartButton.tsx
+++ src/components/ui/CartButton.tsx
@@ -1,14 +1,13 @@
 import cartImage from '../../assets/cart.svg';
+import { RootState, useAppSelector } from '../../store';
 
 export default function CartButton() {
-    const array = Array(100); // later obtained from redux store
+    const { productsAdded } = useAppSelector((state: RootState) => state.cart);
     return (
         <div className="relative cursor-pointer">
-            {array.length > 1 ? (
-                <div
-                    className={`${array.length > 99 ? 'text-xs' : 'text-md'} absolute -left-1 top-1 flex h-5 w-5 items-center justify-center rounded-full bg-accent font-semibold text-white lg:h-7 lg:w-7`}
-                >
-                    {array.length}
+            {productsAdded > 0 ? (
+                <div className="absolute -left-1 top-1 flex h-5 w-5 items-center justify-center rounded-full bg-accent text-xs font-semibold text-white lg:h-7 lg:w-7">
+                    {productsAdded}
                 </div>
             ) : null}
             <img
