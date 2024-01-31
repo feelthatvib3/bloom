fcc22f04b7a669c1dd4f80d0fd270d15d88966b4
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-01-31 22:35:30 +0100

    Add cart reducer to redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
 import { CategoriesSlice } from './features/categoriesSlice';
 import { productsSlice } from './features/productsSlice';
+import cartSlice from './features/cartSlice';
 
 export const store = configureStore({
     reducer: {
         categories: CategoriesSlice.reducer,
         products: productsSlice.reducer,
+        cart: cartSlice,
     },
 });
 
