7fd4c7e651f46984fd3de3105c23305d3be80b6d
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-02 16:20:20 +0100

    Shorten the number of added products when over 999 adding "K+"

--- src/components/ui/CartButton.tsx
+++ src/components/ui/CartButton.tsx
@@ -5,11 +5,13 @@ export default function CartButton() {
     const { productsAdded } = useAppSelector((state: RootState) => state.cart);
     return (
         <div className="relative cursor-pointer">
-            {productsAdded > 0 ? (
+            {productsAdded > 0 && (
                 <div className="absolute -left-1 top-1 flex h-5 w-5 items-center justify-center rounded-full bg-accent text-xs font-semibold text-white lg:h-7 lg:w-7">
-                    {productsAdded}
+                    {productsAdded > 999
+                        ? productsAdded.toString().at(0) + 'K+'
+                        : productsAdded}
                 </div>
-            ) : null}
+            )}
             <img
                 src={cartImage}
                 alt="Shopping Cart"
