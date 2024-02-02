a6ceab679a4653b426c37f66cd10affc4461dd1c
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-02 16:23:07 +0100

    Redirect user to cart page on cart button click

--- src/components/ui/CartButton.tsx
+++ src/components/ui/CartButton.tsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import cartImage from '../../assets/cart.svg';
 import { RootState, useAppSelector } from '../../store';
 
 export default function CartButton() {
     const { productsAdded } = useAppSelector((state: RootState) => state.cart);
     return (
-        <div className="relative cursor-pointer">
+        <Link to="/cart" className="relative cursor-pointer">
             {productsAdded > 0 && (
                 <div className="absolute -left-1 top-1 flex h-5 w-5 items-center justify-center rounded-full bg-accent text-xs font-semibold text-white lg:h-7 lg:w-7">
                     {productsAdded > 999
@@ -17,6 +18,6 @@ export default function CartButton() {
                 alt="Shopping Cart"
                 className="h-8 w-8 lg:h-12 lg:w-11"
             />
-        </div>
+        </Link>
     );
 }
