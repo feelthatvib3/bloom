19564878c91f8bbf3f16f3e5672df834425d5f6b
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-02 22:47:46 +0100

    Replace static close image with corresponding component, fix top padding of navigation items

--- src/components/layout/MobileMenu.tsx
+++ src/components/layout/MobileMenu.tsx
@@ -1,7 +1,7 @@
 import { Dispatch, SetStateAction } from 'react';
 import { Link } from 'react-router-dom';
-import closeButton from '../../assets/close.svg';
 import { navigationLinks } from '../../data/navigationLinks';
+import XButton from '../ui/XButton';
 
 interface MobileMenuProps {
     isMobileMenuOpened: boolean;
@@ -17,17 +17,15 @@ export default function MobileMenu({
             className={`${isMobileMenuOpened ? 'visible opacity-100' : 'invisible opacity-0'} fixed right-0 top-0 z-50 h-full w-full bg-black/10 lg:hidden`}
             onClick={() => setIsMobileMenuOpened(!isMobileMenuOpened)}
         >
-            <div className="ml-auto h-full w-3/5 bg-white pl-8 pr-5 pt-3">
-                <img
-                    src={closeButton}
-                    alt="Close Button"
+            <div className="relative ml-auto h-full w-3/5 bg-white pl-8 pr-5 pt-3">
+                <XButton
                     width={44}
                     height={44}
-                    className="ml-auto cursor-pointer"
+                    className="absolute right-4 top-4"
                     onClick={() => setIsMobileMenuOpened(!isMobileMenuOpened)}
                 />
                 <nav>
-                    <ul className="space-y-9 pt-11">
+                    <ul className="space-y-9 pt-[100px]">
                         {navigationLinks.map(
                             ({ name, href }, index: number) => (
                                 <li key={index}>
