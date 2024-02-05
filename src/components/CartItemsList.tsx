aa13db76a1a157a41372be8a5b2b300fc5606fd0
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-05 16:14:22 +0100

    Create CartItemsList component

---
+++ src/components/CartItemsList.tsx
@@ -0,0 +1,16 @@
+import { CartProduct } from '../store/features/cartSlice';
+import CartItem from './CartItem';
+
+interface CartItemsProps {
+    products: CartProduct[];
+}
+
+export default function CartItemsList({ products }: CartItemsProps) {
+    return (
+        <ul className="grid place-content-start gap-y-3 2xl:col-span-3 2xl:gap-y-4">
+            {products.map((product: CartProduct) => (
+                <CartItem product={product} />
+            ))}
+        </ul>
+    );
+}
