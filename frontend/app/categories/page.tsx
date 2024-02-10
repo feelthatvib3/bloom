58be8821b100402a2ebe0edc37367e0faf616a17
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-11 00:23:53 +0100

    Create categories page

---
+++ frontend/app/categories/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import Container from '@/components/Container';
+
+const CategoriesPage: FC = () => {
+	return (
+		<main className="pt-[calc(1rem+74px)]">
+			<Container>categories page</Container>
+		</main>
+	);
+};
+
+export default CategoriesPage;
