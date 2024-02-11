dc13a7e9ce26e24abccfbd67b20a2055e0a7c4f7
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-11 16:52:03 +0100

    Styles change depending on current pathname & add isScrolled prop

--- frontend/components/navbar/MenuButton.tsx
+++ frontend/components/navbar/MenuButton.tsx
@@ -1,18 +1,23 @@
 import { FC } from 'react';
+import { usePathname } from 'next/navigation';
 
 interface MenuButtonProps {
 	intent: 'open' | 'close';
+	isScrolled?: boolean;
 }
 
-const MenuButton: FC<MenuButtonProps> = ({ intent }) => {
+const MenuButton: FC<MenuButtonProps> = ({ intent, isScrolled }) => {
+	const currentPathname = usePathname();
 	return (
-		<button className="relative flex h-8 w-8 flex-col items-center justify-center bg-lime-100">
+		<button
+			className={`${intent !== 'close' && currentPathname !== '/' && !isScrolled ? 'bg-lime-950' : 'bg-lime-100'} relative flex h-8 w-8 flex-col items-center justify-center `}
+		>
 			{Array(2)
 				.fill(null)
 				.map((_, index) => (
 					<span
 						key={index}
-						className={`absolute h-[1.5px] w-[14px] ${intent === 'open' ? '-translate-y-[3.5px] even:translate-y-[3.5px]' : 'translate-y-0 -rotate-45 even:rotate-45'} bg-lime-950`}
+						className={`${intent !== 'close' && currentPathname !== '/' && !isScrolled ? 'bg-lime-100' : 'bg-lime-950'} ${intent === 'open' ? '-translate-y-[3.5px] even:translate-y-[3.5px]' : 'translate-y-0 -rotate-45 even:rotate-45'} absolute h-[1.5px] w-[14px]`}
 					></span>
 				))}
 		</button>
