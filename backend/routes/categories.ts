08dc5ccf9391b31c7962a23c89693c5f34340d65
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-12 17:55:26 +0100

    Move `:categorySlug` route to `products` router

--- backend/routes/categories.ts
+++ backend/routes/categories.ts
@@ -17,24 +17,4 @@ router.get('/all', async (req: Request, res: Response) => {
 	}
 });
 
-router.get('/:categorySlug', async (req: Request, res: Response) => {
-	try {
-		const { categorySlug } = req.params;
-
-		const categoryWithProducts = await prisma.category.findUnique({
-			where: { slug: categorySlug },
-			include: { products: true },
-		});
-
-		if (!categoryWithProducts) {
-			return res.status(404).json({ error: 'Category not found' });
-		}
-
-		res.json(categoryWithProducts);
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ message: 'Internal server error' });
-	}
-});
-
 export default router;
