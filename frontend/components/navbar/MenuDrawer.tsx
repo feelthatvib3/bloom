d9d0434f42326649bc1ed13a78684a076be2416f
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-12 17:24:51 +0100

    Remove `MenuButton` component to resolve descendants warning

--- frontend/components/navbar/MenuDrawer.tsx
+++ frontend/components/navbar/MenuDrawer.tsx
@@ -18,14 +18,27 @@ import { type IMenuItem, menuItems } from '@/data/menu-items';
 
 interface MenuDrawerProps {
 	isScrolled: boolean;
+	currentPathname: string;
 }
 
-export default function MenuDrawer({ isScrolled }: MenuDrawerProps) {
+export default function MenuDrawer({
+	isScrolled,
+	currentPathname,
+}: MenuDrawerProps) {
 	const [isOpened, setIsOpened] = useState<boolean>(false);
 	return (
 		<Sheet open={isOpened} onOpenChange={setIsOpened}>
-			<SheetTrigger>
-				<MenuButton intent="open" isScrolled={isScrolled} />
+			<SheetTrigger
+				className={`${currentPathname !== '/' && !isScrolled ? 'bg-lime-950' : 'bg-lime-100'} relative flex h-8 w-8 flex-col items-center justify-center `}
+			>
+				{Array(2)
+					.fill(null)
+					.map((_, index) => (
+						<span
+							key={index}
+							className={`${currentPathname !== '/' && !isScrolled ? 'bg-lime-100' : 'bg-lime-950'} absolute h-[1.5px] w-[14px] -translate-y-[3.5px] even:translate-y-[3.5px]`}
+						></span>
+					))}
 			</SheetTrigger>
 			<SheetContent className="border-none bg-lime-950/70 backdrop-blur-md">
 				<SheetHeader className="flex items-center justify-between">
