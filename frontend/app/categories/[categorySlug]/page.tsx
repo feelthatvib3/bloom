a8b1ad4e89933489304138ba61c410a4f9f55b61
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-15 01:06:51 +0100

    Create category products page

--- frontend/app/categories/[categorySlug]/page.tsx
+++ frontend/app/categories/[categorySlug]/page.tsx
@@ -2,13 +2,47 @@
 
 import { useParams } from 'next/navigation';
 
+import Title from '@/components/Title';
 import Container from '@/components/Container';
 
+import { capitalize } from '@/lib/utils';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '@/lib/redux-hooks';
+import { RootState } from '@/store/store';
+import {
+	Product,
+	fetchProductsByCategorySlug,
+} from '@/store/slices/products-slice';
+import { Skeleton } from '@/components/ui/skeleton';
+
 export default function CategoryProducts() {
+	const dispatch = useAppDispatch();
+	const { products, isLoading } = useAppSelector(
+		(state: RootState) => state.products,
+	);
 	const { categorySlug } = useParams();
+	useEffect(() => {
+		dispatch(
+			fetchProductsByCategorySlug(
+				(!Array.isArray(categorySlug) && categorySlug) || '',
+			),
+		);
+	}, [dispatch]);
+	const title = !Array.isArray(categorySlug) && capitalize(categorySlug);
 	return (
-		<main className="pb-[9999px] pt-[calc(1rem+74px)]">
-			<Container>{categorySlug}</Container>
+		<main className="py-[calc(1rem+69px)] lg:py-[calc(2rem+69px)]">
+			<Container>
+				<Title>{title}</Title>
+				<ul className="grid">
+					{isLoading
+						? Array(5)
+								.fill(null)
+								.map((_, index) => (
+									<Skeleton key={index} className="h-[350px] w-[200px]" />
+								))
+						: products.map(({ name }: Product) => <li>{name}</li>)}
+				</ul>
+			</Container>
 		</main>
 	);
 }
