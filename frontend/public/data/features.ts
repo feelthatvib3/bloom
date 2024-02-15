bbee7c7aeca9811ccb29d49bb08561d7fb831baf
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-15 14:33:40 +0100

    Remove icons and add `id` property

--- frontend/public/data/features.ts
+++ frontend/public/data/features.ts
@@ -1,44 +1,32 @@
-import {
-	Squares2X2Icon,
-	RocketLaunchIcon,
-	ReceiptPercentIcon,
-	CreditCardIcon,
-} from '@heroicons/react/24/solid';
-
 export interface Feature {
+	id: number;
 	title: string;
 	description: string;
-	icon: React.ForwardRefExoticComponent<
-		Omit<React.SVGProps<SVGSVGElement>, 'ref'> & {
-			title?: string | undefined;
-			titleId?: string | undefined;
-		} & React.RefAttributes<SVGSVGElement>
-	>;
 }
 
 export const features: Feature[] = [
 	{
+		id: 0,
 		title: 'Seasonal Promotions & Discounts',
 		description:
 			'Enjoy the changing seasons with exclusive promotions and discounts on our garden goods. Stay informed about the latest deals to make the most of your gardening journey while keeping your budget in check.',
-		icon: ReceiptPercentIcon,
 	},
 	{
+		id: 1,
 		title: 'Reliable Shipping',
 		description:
 			'Receive your garden treasures quickly and reliably with our efficient shipping services. Track your order in real-time and anticipate the joy of unwrapping your new plants and accessories in no time.',
-		icon: RocketLaunchIcon,
 	},
 	{
+		id: 2,
 		title: 'Diverse Product Range',
 		description:
 			'Immerse yourself in a vast array of high-quality garden goods, from vibrant flowers and lush plants to durable gardening tools and stylish outdoor décor.',
-		icon: Squares2X2Icon,
 	},
 	{
+		id: 3,
 		title: 'Secure and Convenient Checkout',
 		description:
 			'Experience a hassle-free shopping process with our secure and convenient checkout system. Rest easy knowing that your transactions are protected, and choose from a variety of payment options for added flexibility.',
-		icon: CreditCardIcon,
 	},
 ];
