740bb745792351e87b288dc7ad3091b08e4b62f0
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-18 12:10:30 +0100

    Add `sonner` component from `shadcn` library

---
+++ frontend/components/ui/sonner.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { Toaster as Sonner } from 'sonner';
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+	return (
+		<Sonner
+			className="toaster group"
+			toastOptions={{
+				classNames: {
+					toast:
+						'group-[.toaster]:bg-lime-950 group-[.toaster]:text-lime-200 group-[.toaster]:font-body group-[.toaster]:rounded-none group-[.toaster]:border-none',
+					description: 'group-[.toast]:text-muted-foreground',
+					actionButton:
+						'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
+					cancelButton:
+						'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
+				},
+			}}
+			{...props}
+		/>
+	);
+};
+
+export { Toaster };
