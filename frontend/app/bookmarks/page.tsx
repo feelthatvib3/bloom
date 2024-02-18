5986caf92cde19e037bd8214cb6dcd5ae51356a5
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-18 17:44:16 +0100

    Render different content based on products length

--- frontend/app/bookmarks/page.tsx
+++ frontend/app/bookmarks/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import Container from '@/components/Container';
 import Title from '@/components/Title';
+import Container from '@/components/Container';
+import BookmarksListItem from '@/components/bookmarks/BookmarksListItem';
+
 import { useAppSelector } from '@/lib/redux-hooks';
 import { RootState } from '@/store/store';
 
@@ -10,8 +12,10 @@ export default function BookmarksPage() {
 
 	if (products.length === 0) {
 		return (
-			<Container className="flex h-[62vh] items-center justify-center">
-				<p className="text-xl">You have no bookmarks.</p>
+			<Container className="flex h-[75vh] items-center justify-center">
+				<p className="text-2xl">
+					No bookmarks here. Don't like to save things for later?
+				</p>
 			</Container>
 		);
 	}
@@ -19,9 +23,9 @@ export default function BookmarksPage() {
 	return (
 		<Container className="pb-4 pt-[calc(1rem+69px)] lg:pb-8 lg:pt-[calc(2rem+69px)]">
 			<Title>Bookmarks</Title>
-			<ul>
+			<ul className="mt-8 grid gap-y-4 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-3 xl:grid-cols-4">
 				{products.map((product) => (
-					<li>{product.name}</li>
+					<BookmarksListItem product={product} />
 				))}
 			</ul>
 		</Container>
