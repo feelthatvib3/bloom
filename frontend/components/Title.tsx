bbf29622ea1cb15687eed3f2f63994776bdc2f6c
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-20 22:24:38 +0100

    Increase text on `sm` breakpoint

--- frontend/components/Title.tsx
+++ frontend/components/Title.tsx
@@ -6,5 +6,9 @@ interface TitleProps {
 }
 
 export default function Title({ children, className = '' }: TitleProps) {
-	return <h2 className={`text-4xl lg:text-6xl ${className}`}>{children}</h2>;
+	return (
+		<h2 className={`text-4xl sm:text-5xl lg:text-6xl ${className}`}>
+			{children}
+		</h2>
+	);
 }
