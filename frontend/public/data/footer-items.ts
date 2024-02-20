1f6c758e098f75da97b2150aec6bdb9e6efd071a
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-20 12:40:37 +0100

    Add footer items

--- frontend/public/data/footer-items.ts
+++ frontend/public/data/footer-items.ts
@@ -1,27 +1,90 @@
 export interface FooterCategory {
+	id: number;
 	title: string;
 	items: FooterCategoryItem[];
 }
 
 export interface FooterCategoryItem {
+	id: number;
 	label: string;
 	href: string;
 }
 
 export const footerItems: FooterCategory[] = [
 	{
+		id: 0,
+		title: 'Service',
+		items: [
+			{
+				id: 0,
+				label: 'Contact',
+				href: '#',
+			},
+			{
+				id: 1,
+				label: 'Order status',
+				href: '#',
+			},
+			{
+				id: 2,
+				label: 'Complaints',
+				href: '#',
+			},
+			{
+				id: 3,
+				label: 'Report an error',
+				href: '#',
+			},
+		],
+	},
+	{
+		id: 1,
+		title: 'Shopping',
+		items: [
+			{
+				id: 0,
+				label: 'Expert guides',
+				href: '#',
+			},
+			{
+				id: 1,
+				label: 'Manufacturers',
+				href: '#',
+			},
+			{
+				id: 2,
+				label: 'Payment options',
+				href: '#',
+			},
+			{
+				id: 3,
+				label: 'Partnership program',
+				href: '#',
+			},
+		],
+	},
+	{
+		id: 2,
 		title: 'Legal',
 		items: [
 			{
+				id: 0,
 				label: 'Security',
 				href: '#',
 			},
 			{
-				label: 'Terms of Use',
+				id: 1,
+				label: 'Regulations',
 				href: '#',
 			},
 			{
-				label: 'Privacy Policy',
+				id: 2,
+				label: 'Terms of use',
+				href: '#',
+			},
+			{
+				id: 3,
+				label: 'Privacy policy',
 				href: '#',
 			},
 		],
