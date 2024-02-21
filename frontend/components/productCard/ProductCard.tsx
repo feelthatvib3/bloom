7ea5139f4d4d423a2b02fe1937c12ec3a27063e6
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-21 14:39:08 +0100

    Replace `router` with `Link` component to navigate to the product page

--- frontend/components/productCard/ProductCard.tsx
+++ frontend/components/productCard/ProductCard.tsx
@@ -1,14 +1,14 @@
 'use client';
 
+import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 import ProductPrice from '@/components/productCard/ProductPrice';
 import BookmarkButton from '@/components/productCard/BookmarkButton';
 import AddToCartButton from '@/components/productCard/AddToCartButton';
 
 import { ROOT_URL } from '@/store/store';
-import { Product } from '@/store/slices/products-slice';
+import { type Product } from '@/store/types';
 
 interface ProductCardProps {
 	product: Product;
@@ -16,52 +16,50 @@ interface ProductCardProps {
 
 export default function ProductCard({ product }: ProductCardProps) {
 	const { id, categoryTitle, name, price, discount, image } = product;
-	const router = useRouter();
 	return (
-		<li
-			onClick={() => router.push(categoryTitle.toLowerCase() + '/' + id)}
-			className="min-w-xs relative min-h-[450px] cursor-pointer p-4 text-lime-200"
-		>
-			{/* category title badge */}
-			<span className="absolute right-4 top-4 bg-lime-200 p-1 font-medium text-lime-950">
-				{categoryTitle}
-			</span>
+		<li className="min-w-xs relative min-h-[450px] cursor-pointer text-lime-200">
+			<Link href={`/products/${id}`} className="block p-4">
+				{/* category title badge */}
+				<span className="absolute right-4 top-4 bg-lime-200 p-1 font-medium text-lime-950">
+					{categoryTitle}
+				</span>
 
-			{/* background image */}
-			<Image
-				src={ROOT_URL + image}
-				alt={name}
-				width={320}
-				height={320}
-				className="absolute left-0 top-0 -z-20 h-full w-full object-cover"
-				quality={0}
-			/>
+				{/* background image */}
+				<Image
+					src={ROOT_URL + image}
+					alt={name}
+					width={320}
+					height={320}
+					className="absolute left-0 top-0 -z-20 h-full w-full object-cover"
+					quality={0}
+				/>
 
-			{/* overlay */}
-			<div className="absolute left-0 top-0 -z-10 h-full w-full bg-lime-950/80 backdrop-blur-xl transition-colors group-hover:bg-lime-200/70"></div>
+				{/* overlay */}
+				<div className="absolute left-0 top-0 -z-10 h-full w-full bg-lime-950/80 backdrop-blur-xl transition-colors group-hover:bg-lime-200/70"></div>
 
-			{/* content */}
-			<div>
+				{/* content */}
 				<div>
-					<Image
-						src={ROOT_URL + image}
-						alt={name}
-						width={300}
-						height={300}
-						className="h-full w-full object-cover"
-					/>
-				</div>
-				<div>
-					<p className="line-clamp-1 text-4xl font-medium">{name}</p>
-					<div className="mt-6">
-						<ProductPrice regularPrice={price} discountPercent={discount} />
-						<div className="mt-2 flex gap-x-2">
-							<AddToCartButton product={product} />
-							<BookmarkButton product={product} />
+					<div>
+						<Image
+							src={ROOT_URL + image}
+							alt={name}
+							width={300}
+							height={300}
+							className="h-full w-full object-cover"
+						/>
+					</div>
+					<div>
+						<p className="line-clamp-1 text-4xl font-medium">{name}</p>
+						<div className="mt-6">
+							<ProductPrice regularPrice={price} discountPercent={discount} />
+							<div className="mt-2 flex gap-x-2">
+								<AddToCartButton product={product} />
+								<BookmarkButton product={product} />
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</Link>
 		</li>
 	);
 }
