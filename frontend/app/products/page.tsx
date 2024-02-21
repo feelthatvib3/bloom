e459f2dfa36d736a456fd2404869ceaa3d2c4224
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-21 14:13:32 +0100

    Add `ProductsFilter` component to `ProductsPage`

--- frontend/app/products/page.tsx
+++ frontend/app/products/page.tsx
@@ -1,12 +1,19 @@
 import Title from '@/components/Title';
 import Container from '@/components/Container';
 import ProductList from '@/components/ProductsList';
+import ProductsFilter from '@/components/ProductsFilter';
+
+import { ROOT_URL } from '@/store/store';
 
 export default function ProductsPage() {
+	const endpoint = ROOT_URL + 'products/all';
 	return (
 		<Container className="pb-4 pt-[calc(1rem+69px)] lg:pb-8 lg:pt-[calc(2rem+69px)]">
 			<Title>All products</Title>
-			<ProductList filterType="all" />
+			<div className="mb-8 mt-6">
+				<ProductsFilter endpoint={endpoint} />
+			</div>
+			<ProductList />
 		</Container>
 	);
 }
