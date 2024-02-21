c3b16e3caef34e088efb03ffeefef0f2cb4b3668
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-21 14:38:36 +0100

    Remove file

--- frontend/app/categories/[categorySlug]/[productId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Container from '@/components/Container';
-
-export default function ProductPage() {
-	return (
-		<main className="p-4 pt-[calc(1rem+69px)] lg:pb-8 lg:pt-[calc(2rem+69px)]">
-			<Container>product page</Container>
-		</main>
-	);
-}
