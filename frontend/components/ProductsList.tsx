39757ab2823089180a4237df45fb58e71590cb12
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-21 14:11:54 +0100

    Display `No products found.` if no products fetched & update import path

--- frontend/components/ProductsList.tsx
+++ frontend/components/ProductsList.tsx
@@ -1,58 +1,36 @@
 'use client';
 
-import { useEffect } from 'react';
-
 import { Skeleton } from '@/components/ui/skeleton';
 import ProductCard from '@/components/productCard/ProductCard';
 
 import { RootState } from '@/store/store';
-import {
-	Product,
-	fetchAllProducts,
-	fetchProductsByCategorySlug,
-} from '@/store/slices/products-slice';
-import { useAppDispatch, useAppSelector } from '@/lib/redux-hooks';
+import { type Product } from '@/store/types';
+import { useAppSelector } from '@/lib/redux-hooks';
 
-type ProductsListProps =
-	| {
-			filterType: 'category';
-			categorySlug: string;
-	  }
-	| { filterType: 'all' | 'sale' };
-
-export default function ProductList({
-	filterType,
-	// @ts-ignore
-	categorySlug,
-}: ProductsListProps) {
+export default function ProductList() {
 	const { products, isLoading } = useAppSelector(
 		(state: RootState) => state.products,
 	);
-	const dispatch = useAppDispatch();
-
-	useEffect(() => {
-		switch (filterType) {
-			case 'all':
-				dispatch(fetchAllProducts());
-				break;
-			case 'category':
-				dispatch(fetchProductsByCategorySlug(categorySlug));
-				break;
-			default:
-				break;
-		}
-	}, [dispatch]);
 	return (
-		<ul className="mt-8 grid gap-y-4 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-3 xl:grid-cols-4">
-			{isLoading
-				? Array(8)
-						.fill(null)
-						.map((_, index) => (
-							<Skeleton key={index} className="h-[470px] w-[300px]" />
-						))
-				: products.map((product: Product) => (
-						<ProductCard key={product.id} product={product} />
-					))}
+		<ul className="grid gap-y-4 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-3 xl:grid-cols-4">
+			{isLoading ? (
+				Array(8)
+					.fill(null)
+					.map((_, index) => (
+						<Skeleton
+							key={index}
+							className="h-[500px] w-full lg:h-[470px] lg:w-[300px]"
+						/>
+					))
+			) : products.length === 0 ? (
+				<p className="col-span-full text-center text-lg font-medium lg:text-start">
+					No products found.
+				</p>
+			) : (
+				products.map((product: Product) => (
+					<ProductCard key={product.id} product={product} />
+				))
+			)}
 		</ul>
 	);
 }
