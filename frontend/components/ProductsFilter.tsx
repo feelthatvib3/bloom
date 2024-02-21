ffde6c4861aad67df78cebc5753ea3a2ecb01d2d
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-21 14:12:11 +0100

    Refactor

--- frontend/components/ProductsFilter.tsx
+++ frontend/components/ProductsFilter.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { CheckIcon } from '@heroicons/react/24/solid';
+import { ChangeEvent, useEffect, useState } from 'react';
 
-import {
-	FilterOptions,
-	SortType,
-	fetchProducts,
-} from '@/store/slices/products-slice';
 import { useAppDispatch } from '@/lib/redux-hooks';
+import { type FilterOptions, type SortType } from '@/store/types';
+import { fetchProducts, startLoading } from '@/store/slices/products-slice';
 
 import {
 	Select,
@@ -18,61 +15,68 @@ import {
 	SelectValue,
 } from '@/components/ui/select';
 
+import { selectValues } from '@/public/data/filter-select-items';
+
 interface ProductsFilterProp {
 	endpoint: string;
 }
 
 export default function ProductsFilter({ endpoint }: ProductsFilterProp) {
+	const dispatch = useAppDispatch();
+
 	const [fromPrice, setFromPrice] = useState<number>(0);
-	const [toPrice, setToPrice] = useState<number>(Infinity);
+	const [toPrice, setToPrice] = useState<number>(Number.MAX_SAFE_INTEGER);
 	const [isDiscounted, setIsDiscounted] = useState<boolean>(false);
 	const [sortBy, setSortBy] = useState<SortType>('newest');
 
-	const selectValues: { value: SortType; label: string }[] = [
-		{
-			value: 'newest',
-			label: 'Newest',
-		},
-		{
-			value: 'oldest',
-			label: 'Oldest',
-		},
-		{
-			value: 'alphabetical',
-			label: 'A-Z',
-		},
-		{
-			value: 'reverseAlphabetical',
-			label: 'Z-A',
-		},
-	];
+	const handleFromPriceChange = (event: ChangeEvent<HTMLInputElement>) =>
+		setFromPrice(+event.target.value);
+
+	const handleToPriceChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const toPrice = +event.target.value;
+		toPrice === 0 ? setToPrice(Number.MAX_SAFE_INTEGER) : setToPrice(toPrice);
+	};
 
 	const handleSortChange = (value: SortType) => setSortBy(value);
 
-	const filters: FilterOptions = {
-		fromPrice,
-		toPrice,
-		isDiscounted,
-		sortBy,
-	};
+	const handleIsDiscountedChange = (event: ChangeEvent<HTMLInputElement>) =>
+		setIsDiscounted(event.target.checked);
 
-	const dispatch = useAppDispatch();
 	useEffect(() => {
-		dispatch(fetchProducts({ endpoint, filters }));
-	}, [dispatch, fromPrice, toPrice, isDiscounted, sortBy]);
+		const fetchData = (endpoint: string, filters: FilterOptions) =>
+			dispatch(fetchProducts({ endpoint, filters }));
+		const startLoadingData = () => dispatch(startLoading());
+
+		startLoadingData();
+
+		const timeoutId = setTimeout(() => {
+			const filters: FilterOptions = {
+				fromPrice,
+				toPrice,
+				isDiscounted,
+				sortBy,
+			};
+
+			fetchData(endpoint, filters);
+		}, 1500);
+
+		return () => {
+			clearTimeout(timeoutId);
+		};
+	}, [dispatch, fromPrice, toPrice, isDiscounted, sortBy, endpoint]);
 	return (
 		<form className="flex flex-col items-start gap-y-3 sm:flex-row sm:gap-x-4 sm:gap-y-0">
 			<div className="space-y-1">
 				<span className="text-xl font-medium">Price</span>
 				<div className="space-x-2">
 					<input
-						onChange={(e) => setFromPrice(+e.target.value)}
+						onChange={handleFromPriceChange}
 						type="number"
 						placeholder="from"
 						className="h-[36px] max-w-[125px] border border-lime-950 bg-transparent px-2 py-1 text-lg outline-none placeholder:text-lime-950/65"
 					/>
 					<input
-						onChange={(e) => setToPrice(+e.target.value)}
+						onChange={handleToPriceChange}
 						type="number"
 						placeholder="to"
 						className="h-[36px] max-w-[125px] border border-lime-950 bg-transparent p-1 px-2 text-lg outline-none placeholder:text-lime-950/65"
@@ -86,9 +90,9 @@ export default function ProductsFilter({ endpoint }: ProductsFilterProp) {
 						<SelectValue />
 					</SelectTrigger>
 					<SelectContent className="w-[180px] rounded-none border-none bg-lime-950/65 backdrop-blur-xl">
-						{selectValues.map(({ value, label }, idx) => (
+						{selectValues.map(({ value, label }, index) => (
 							<SelectItem
-								key={idx}
+								key={index}
 								className="text-lg text-lime-200 focus:rounded-none focus:bg-lime-200/25 focus:text-lime-200"
 								value={value}
 							>
@@ -102,7 +106,7 @@ export default function ProductsFilter({ endpoint }: ProductsFilterProp) {
 				<span className="text-xl font-medium">Discounted</span>
 				<div className="relative h-[36px] w-[36px]">
 					<input
-						onChange={(e) => setIsDiscounted(e.target.checked)}
+						onChange={handleIsDiscountedChange}
 						type="checkbox"
 						className="group relative h-full w-full cursor-pointer appearance-none rounded-none border border-lime-950 outline-none transition-colors checked:bg-lime-950"
 					/>
