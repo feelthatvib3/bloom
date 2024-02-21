6978592ef925604f1f695fd0532d6b1374154d61
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-21 14:25:25 +0100

    Styles fix

--- frontend/components/footer/Footer.tsx
+++ frontend/components/footer/Footer.tsx
@@ -12,11 +12,11 @@ import FooterCategory from '@/components/footer/FooterCategory';
 export default function Footer() {
 	return (
 		<footer className="py-6 md:py-10 lg:py-14">
-			<Container className="flex flex-col justify-between gap-y-8 sm:items-center md:flex-row md:items-start lg:gap-y-0">
+			<Container className="flex flex-col items-center justify-between gap-y-8 md:flex-row md:items-start lg:gap-y-0">
 				<Link href="/" className="font-heading text-3xl uppercase">
 					Bloom
 				</Link>
-				<div className="flex flex-col gap-y-4 sm:flex-row sm:gap-x-16">
+				<div className="flex flex-col justify-center gap-x-8 gap-y-8 sm:flex-row sm:gap-x-14 md:gap-x-16">
 					{footerItems.map((footerCategory: FooterCategoryType) => (
 						<FooterCategory key={footerCategory.id} category={footerCategory} />
 					))}
