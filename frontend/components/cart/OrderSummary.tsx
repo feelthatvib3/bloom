560fa4e81bc13e33e6349effecc771e9a88317ed
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-22 19:41:18 +0100

    Clear cart and pass data to the backend on `Checkout` button click

--- frontend/components/cart/OrderSummary.tsx
+++ frontend/components/cart/OrderSummary.tsx
@@ -1,13 +1,62 @@
+'use client';
+
+import { useState } from 'react';
+import { CheckIcon, ExclamationTriangleIcon } from '@heroicons/react/24/solid';
+
+import { toast } from 'sonner';
+
 import Button from '@/components/Button';
 
-import { useAppSelector } from '@/lib/redux-hooks';
-import { RootState } from '@/store/store';
+import { Product } from '@/store/types';
+import { ROOT_URL, RootState } from '@/store/store';
+import { clearCart } from '@/store/slices/cart-slice';
+import { useAppDispatch, useAppSelector } from '@/lib/redux-hooks';
 
 export default function OrderSummary() {
-	const { totalPrice } = useAppSelector((state: RootState) => state.cart);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	const { products, totalPrice } = useAppSelector(
+		(state: RootState) => state.cart,
+	);
+	const dispatch = useAppDispatch();
+
 	const subtotal = totalPrice;
 	const tax = 3;
 	const total = totalPrice + tax;
+
+	const postOrder = async (order: Product[]) => {
+		const response = await fetch(ROOT_URL + 'order/new', {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			method: 'POST',
+			body: JSON.stringify(order),
+		});
+
+		if (!response.ok) throw new Error(response.statusText);
+
+		const data = await response.json();
+		return data.message;
+	};
+
+	const handleCheckout = async () => {
+		try {
+			setIsLoading(true);
+
+			const responseMessage = await postOrder(products);
+			dispatch(clearCart());
+
+			toast(responseMessage, {
+				icon: <CheckIcon />,
+			});
+		} catch (error: any) {
+			toast(error.message, {
+				icon: <ExclamationTriangleIcon />,
+			});
+		} finally {
+			setIsLoading(false);
+		}
+	};
 	return (
 		<div className="flex h-fit flex-col gap-y-4 border border-lime-950 p-4 lg:w-2/5">
 			<p className="text-2xl font-semibold uppercase">Order summary</p>
@@ -25,7 +74,11 @@ export default function OrderSummary() {
 					<p className="font-medium">${total.toFixed(2)}</p>
 				</div>
 			</div>
-			<Button label="Checkout" />
+			<Button
+				disabled={isLoading}
+				label={isLoading ? 'Loading...' : 'Checkout'}
+				onClick={handleCheckout}
+			/>
 		</div>
 	);
 }
