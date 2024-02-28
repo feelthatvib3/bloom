a4cfd457ff3b26835e6e9866b35b7f1e83805b0f
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-28 22:50:41 +0100

    Add modal for checkout

--- frontend/app/cart/components/OrderSummary.tsx
+++ frontend/app/cart/components/OrderSummary.tsx
@@ -1,86 +1,68 @@
 'use client';
 
-import type { Product, RootState } from '@/app/lib/definitions';
+import type { RootState } from '@/app/lib/definitions';
 
-import { toast } from 'sonner';
 import { useState } from 'react';
-import { CheckIcon, ExclamationTriangleIcon } from '@heroicons/react/24/solid';
-
-import { ROOT_URL } from '@/app/lib/constants';
 
 import Button from '@/app/ui/Button';
+import CheckoutForm from '@/app/cart/components/CheckoutForm';
 
-import { clearCart } from '@/store/slices/cart-slice';
-import { useAppDispatch, useAppSelector } from '@/app/lib/redux-hooks';
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogTrigger,
+} from '@/components/ui/dialog';
+
+import { useAppSelector } from '@/app/lib/redux-hooks';
+import { XMarkIcon } from '@heroicons/react/24/solid';
 
 export default function OrderSummary() {
-	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
 
 	const { products, totalPrice } = useAppSelector(
 		(state: RootState) => state.cart,
 	);
-	const dispatch = useAppDispatch();
 
 	const subtotal = totalPrice;
 	const tax = 3;
 	const total = totalPrice + tax;
-
-	const postOrder = async (order: Product[]) => {
-		const response = await fetch(ROOT_URL + '/order/new', {
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			method: 'POST',
-			body: JSON.stringify(order),
-		});
-
-		if (!response.ok) throw new Error(response.statusText);
-
-		const data = await response.json();
-		return data.message;
-	};
-
-	const handleCheckout = async () => {
-		try {
-			setIsLoading(true);
-
-			const responseMessage = await postOrder(products);
-			dispatch(clearCart());
-
-			toast(`${responseMessage}.`, {
-				icon: <CheckIcon />,
-			});
-		} catch (error: any) {
-			const errorMessage = error.message;
-			toast(`${errorMessage}.`, {
-				icon: <ExclamationTriangleIcon />,
-			});
-		} finally {
-			setIsLoading(false);
-		}
-	};
 	return (
-		<div className="flex h-fit flex-col gap-y-4 border border-lime-950 p-4 lg:w-2/5">
-			<p className="text-2xl font-semibold uppercase">Order summary</p>
-			<div className="text-lg">
-				<div className="flex justify-between gap-x-4">
-					<p className="font-medium uppercase text-lime-950/65">Subtotal</p>
-					<p className="font-medium">${subtotal.toFixed(2)}</p>
+		<>
+			<div className="flex h-fit flex-col gap-y-4 border border-lime-950 p-4 lg:w-2/5">
+				<p className="text-2xl font-semibold uppercase">Order summary</p>
+				<div className="text-lg">
+					<div className="flex justify-between gap-x-4">
+						<p className="font-medium uppercase text-lime-950/65">Subtotal</p>
+						<p className="font-medium">${subtotal.toFixed(2)}</p>
+					</div>
+					<div className="flex justify-between gap-x-4">
+						<p className="font-medium uppercase text-lime-950/65">Tax</p>
+						<p className="font-medium">${tax.toFixed(2)}</p>
+					</div>
+					<div className="flex justify-between gap-x-4">
+						<p className="font-medium uppercase text-lime-950/65">Total</p>
+						<p className="font-medium">${total.toFixed(2)}</p>
+					</div>
 				</div>
-				<div className="flex justify-between gap-x-4">
-					<p className="font-medium uppercase text-lime-950/65">Tax</p>
-					<p className="font-medium">${tax.toFixed(2)}</p>
-				</div>
-				<div className="flex justify-between gap-x-4">
-					<p className="font-medium uppercase text-lime-950/65">Total</p>
-					<p className="font-medium">${total.toFixed(2)}</p>
-				</div>
+				<Dialog open={isModalOpened} onOpenChange={setIsModalOpened}>
+					<DialogTrigger asChild>
+						<Button label="Checkout" />
+					</DialogTrigger>
+					<DialogContent className="!rounded-none border-lime-100 bg-lime-100 sm:max-w-[425px]">
+						<div className="flex flex-row items-center justify-between">
+							<p className="text-3xl font-semibold uppercase">Checkout</p>
+							<DialogClose className="relative flex flex-col items-center justify-center bg-transparent p-1 transition-colors hover:bg-lime-700/10">
+								<XMarkIcon className="h-7 w-7 text-lime-950/85" />
+							</DialogClose>
+						</div>
+						<CheckoutForm
+							order={products}
+							setIsModalOpened={setIsModalOpened}
+						/>
+					</DialogContent>
+				</Dialog>
 			</div>
-			<Button
-				disabled={isLoading}
-				label={isLoading ? 'Loading...' : 'Checkout'}
-				onClick={handleCheckout}
-			/>
-		</div>
+		</>
 	);
 }
