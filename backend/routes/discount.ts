97ca022f3b4ec2340b7404f9e04f595d1809c635
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-03-02 18:34:23 +0100

    Move `backend` directory to dedicated branch

--- backend/routes/discount.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router, Request, Response } from 'express';
-
-const router: Router = express.Router();
-
-router.post('/new', (req: Request, res: Response) => {
-	res.status(200).json({ message: 'Request submitted' });
-});
-
-export default router;
